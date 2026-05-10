de6f23a91ff3f4cd165f01d1b08beb0468a5eac4
Vaughn <50187046+cyb3rclon3@users.noreply.github.com>
2026-05-09 22:15:42 -0400

    Write posts for inverter, dyno

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
-import { defineConfig, squooshImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-  image: {
-    service: squooshImageService(),
-  },
   integrations: [tailwind(), react()],
 });
